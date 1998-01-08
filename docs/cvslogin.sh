bd9a60aa7f754d8513287c2de23064369ca2323a
cout <cout>
1998-01-08 09:34:47 +0000

    Initial import.

---
+++ docs/cvslogin.sh
@@ -0,0 +1,17 @@
+#!/bin/sh
+
+echo "Please be sure to run this program as \"source cvslogin <username>\","
+echo "otherwise your environment variables may not be set correctly."
+
+if [ "$1" == "" ]; then
+  echo "Usage: cvslogin <username>"
+  exit
+fi;
+
+CVS_RSH=ssh
+CVSROOT=:ext:$1@sx3.dhs.org:/cvs/sx3
+
+export CVS_RSH CVSROOT
+
+echo "Finished.  Please check your environment variables to make sure that"
+echo "CVS_RSH and CVSROOT are set properly."
