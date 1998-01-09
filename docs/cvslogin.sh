d0d9dd9077beca6aa53b6dd82b5c9e2a49143a60
cout <cout>
1998-01-09 08:27:29 +0000

    Fixed cvslogin.sh to not exit from the parent shell when no parameters are specified.

--- docs/cvslogin.sh
+++ docs/cvslogin.sh
@@ -4,14 +4,17 @@ echo "Please be sure to run this program as \"source cvslogin <username>\","
 echo "otherwise your environment variables may not be set correctly."
 
 if [ "$1" == "" ]; then
+
   echo "Usage: cvslogin <username>"
-  exit
+
+else
+
+  CVS_RSH=ssh
+  CVSROOT=:ext:$1@sx3.dhs.org:/cvs/sx3
+
+  export CVS_RSH CVSROOT
+
+  echo "Finished.  Please check your environment variables to make sure that"
+  echo "CVS_RSH and CVSROOT are set properly."
+
 fi;
-
-CVS_RSH=ssh
-CVSROOT=:ext:$1@sx3.dhs.org:/cvs/sx3
-
-export CVS_RSH CVSROOT
-
-echo "Finished.  Please check your environment variables to make sure that"
-echo "CVS_RSH and CVSROOT are set properly."
