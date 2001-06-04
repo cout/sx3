ab723dd2a2353a7f34ba4e4c64f3ae0e3fe3b0b8
cout <cout>
2001-06-04 14:53:43 +0000

    Changed tabs to spaces.  Added an init_font function, which should make adding Win32 support and adding additional fonts easier.  Added a check to make sure that malloc() succeeds and return an error code if it does.  Switched the X font names to map to the real font instead of something that just looks right.  Added GLT_FIXED.

--- gltext/Makefile.d
+++ gltext/Makefile.d
@@ -1,8 +1,8 @@
 test.o: test.c /usr/include/SDL/SDL.h /usr/include/SDL/SDL_main.h \
- /usr/include/SDL/SDL_types.h /usr/include/SDL/SDL_error.h \
- /usr/include/SDL/begin_code.h /usr/include/SDL/close_code.h \
- /usr/include/SDL/SDL_rwops.h /usr/include/stdio.h \
- /usr/include/features.h /usr/include/sys/cdefs.h \
+ /usr/include/SDL/SDL_types.h /usr/include/SDL/SDL_getenv.h \
+ /usr/include/SDL/SDL_error.h /usr/include/SDL/begin_code.h \
+ /usr/include/SDL/close_code.h /usr/include/SDL/SDL_rwops.h \
+ /usr/include/stdio.h /usr/include/features.h /usr/include/sys/cdefs.h \
  /usr/include/gnu/stubs.h \
  /usr/lib/gcc-lib/i586-mandrake-linux/2.95.3/include/stddef.h \
  /usr/lib/gcc-lib/i586-mandrake-linux/2.95.3/include/stdarg.h \
@@ -15,15 +15,15 @@ test.o: test.c /usr/include/SDL/SDL.h /usr/include/SDL/SDL_main.h \
  /usr/include/SDL/SDL_keysym.h /usr/include/SDL/SDL_mouse.h \
  /usr/include/SDL/SDL_video.h /usr/include/SDL/SDL_mutex.h \
  /usr/include/SDL/SDL_quit.h /usr/include/SDL/SDL_version.h \
- /usr/include/GL/gl.h /usr/include/GL/glext.h /usr/include/GL/glu.h \
- /usr/include/stdlib.h /usr/include/sys/types.h /usr/include/time.h \
- /usr/include/endian.h /usr/include/bits/endian.h \
- /usr/include/sys/select.h /usr/include/bits/select.h \
- /usr/include/bits/sigset.h /usr/include/sys/sysmacros.h \
- /usr/include/alloca.h gltext.h /usr/include/GL/glx.h \
- /usr/include/X11/Xlib.h /usr/include/X11/X.h \
- /usr/include/X11/Xfuncproto.h /usr/include/X11/Xosdefs.h \
- /usr/include/X11/Xutil.h /usr/include/GL/glxtokens.h
+ /usr/local/include/GL/gl.h /usr/local/include/GL/glext.h \
+ /usr/local/include/GL/glu.h /usr/include/stdlib.h \
+ /usr/include/sys/types.h /usr/include/time.h /usr/include/endian.h \
+ /usr/include/bits/endian.h /usr/include/sys/select.h \
+ /usr/include/bits/select.h /usr/include/bits/sigset.h \
+ /usr/include/sys/sysmacros.h /usr/include/alloca.h gltext.h \
+ /usr/local/include/GL/glx.h /usr/include/X11/Xlib.h \
+ /usr/include/X11/X.h /usr/include/X11/Xfuncproto.h \
+ /usr/include/X11/Xosdefs.h /usr/include/X11/Xutil.h
 gltext.o: gltext.c /usr/include/stdlib.h /usr/include/features.h \
  /usr/include/sys/cdefs.h /usr/include/gnu/stubs.h \
  /usr/lib/gcc-lib/i586-mandrake-linux/2.95.3/include/stddef.h \
@@ -31,8 +31,9 @@ gltext.o: gltext.c /usr/include/stdlib.h /usr/include/features.h \
  /usr/include/time.h /usr/include/endian.h /usr/include/bits/endian.h \
  /usr/include/sys/select.h /usr/include/bits/select.h \
  /usr/include/bits/sigset.h /usr/include/sys/sysmacros.h \
- /usr/include/alloca.h /usr/include/GL/glut.h /usr/include/GL/gl.h \
- /usr/include/GL/glext.h /usr/include/GL/glu.h /usr/include/GL/glx.h \
+ /usr/include/alloca.h /usr/local/include/GL/glut.h \
+ /usr/local/include/GL/gl.h /usr/local/include/GL/glext.h \
+ /usr/local/include/GL/glu.h /usr/local/include/GL/glx.h \
  /usr/include/X11/Xlib.h /usr/include/X11/X.h \
  /usr/include/X11/Xfuncproto.h /usr/include/X11/Xosdefs.h \
- /usr/include/X11/Xutil.h /usr/include/GL/glxtokens.h gltext.h
+ /usr/include/X11/Xutil.h gltext.h
