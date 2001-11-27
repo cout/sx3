8d220668f720dd0ccac74a8a2392ed6419cd19d1
cout <cout>
2001-11-27 05:29:03 +0000

    Use the CTL.

--- matrix/matrix.h
+++ matrix/matrix.h
@@ -3,6 +3,7 @@
 
 #include <math.h>
 #include <string.h>
+#include "ctl/c_util.h"
 
 /* Matrix math */
 /* Note:
@@ -17,18 +18,8 @@ typedef float Matrix[16];
 /*typedef float* pMatrix;*/
 #define pMatrix float*
 
-#ifdef __cplusplus
-#define INLINE inline
-#else
-#if defined __STDC_VERSION__ && __STDC_VERSION__ >= 199401
-#define INLINE static inline
-#else
-#define INLINE static __attribute__((unused))
-#endif
-#endif
-
 /* vv_add adds two vectors, a and b, and places the result in a */
-INLINE void vv_add(pVector a, const pVector b) {
+CTL_INLINE void vv_add(pVector a, const pVector b) {
     a[0] += b[0];
     a[1] += b[1];
     a[2] += b[2];
@@ -36,7 +27,7 @@ INLINE void vv_add(pVector a, const pVector b) {
 }
 
 /* vv_sub subtracts two vectors, a and b, and places the result in a */
-INLINE void vv_sub(pVector a, const pVector b) {
+CTL_INLINE void vv_sub(pVector a, const pVector b) {
     a[0] -= b[0];
     a[1] -= b[1];
     a[2] -= b[2];
@@ -44,7 +35,7 @@ INLINE void vv_sub(pVector a, const pVector b) {
 }
 
 /* vv_div divides two vectors, a and b, and places the result in a */
-INLINE void vv_div(pVector a, const pVector b) {
+CTL_INLINE void vv_div(pVector a, const pVector b) {
     a[0] /= b[0];
     a[1] /= b[1];
     a[2] /= b[2];
@@ -52,7 +43,7 @@ INLINE void vv_div(pVector a, const pVector b) {
 }
 
 /* vv_div divides two vectors, a and b, and places the result in a */
-INLINE void vv_mul(pVector a, const pVector b) {
+CTL_INLINE void vv_mul(pVector a, const pVector b) {
     a[0] *= b[0];
     a[1] *= b[1];
     a[2] *= b[2];
@@ -60,17 +51,17 @@ INLINE void vv_mul(pVector a, const pVector b) {
 }
 
 /* vv_dot finds the dot product of a and b, and returns the result */
-INLINE double vv_dot(const pVector a, const pVector b) {
+CTL_INLINE double vv_dot(const pVector a, const pVector b) {
     return a[0]*b[0] + a[1]*b[1] + a[2]*b[2];
 }
 
 /* vv_cpy sets vector dest = vector src */
-INLINE void vv_cpy(pVector dest, const pVector src) {
+CTL_INLINE void vv_cpy(pVector dest, const pVector src) {
     memcpy(dest, src, sizeof(Vector));
 }
 
 /* vv_cross finds the cross product of a and b, and places the result in a */
-INLINE void vv_cross(pVector a, const pVector b) {
+CTL_INLINE void vv_cross(pVector a, const pVector b) {
     Vector result;
     result[0] = a[1] * b[2] - a[2] * b[1];
     result[1] = a[2] * b[0] - a[0] * b[2];
@@ -79,7 +70,7 @@ INLINE void vv_cross(pVector a, const pVector b) {
 }
 
 /* vc_add adds a constant b to vector a */
-INLINE void vc_add(pVector a, double b) {
+CTL_INLINE void vc_add(pVector a, double b) {
     a[0] += b;
     a[1] += b;
     a[2] += b;
@@ -87,7 +78,7 @@ INLINE void vc_add(pVector a, double b) {
 }
 
 /* vc_mul multiples a vector a by a constant b */
-INLINE void vc_mul(pVector a, double b) {
+CTL_INLINE void vc_mul(pVector a, double b) {
     a[0] *= b;
     a[1] *= b;
     a[2] *= b;
@@ -95,7 +86,7 @@ INLINE void vc_mul(pVector a, double b) {
 }
 
 /* vc_div divides a vector a by a constant b */
-INLINE void vc_div(pVector a, double b) {
+CTL_INLINE void vc_div(pVector a, double b) {
     a[0] /= b;
     a[1] /= b;
     a[2] /= b;
@@ -103,7 +94,7 @@ INLINE void vc_div(pVector a, double b) {
 }
 
 /* v_exp places e^a in a */
-INLINE void v_exp(pVector a) {
+CTL_INLINE void v_exp(pVector a) {
     a[0] = exp(a[0]);
     a[1] = exp(a[1]);
     a[2] = exp(a[2]);
@@ -111,30 +102,30 @@ INLINE void v_exp(pVector a) {
 }
 
 /* v_zero sets a to zero */
-INLINE void v_zero(pVector a) {
+CTL_INLINE void v_zero(pVector a) {
     a[0] = a[1] = a[2] = a[3] = 0;
 }
 
 /* v_mag finds the magnitude of a vector */
-INLINE float v_mag(pVector a) {
+CTL_INLINE float v_mag(pVector a) {
     return sqrt(vv_dot(a, a));
 }
 
 /* v_norm normalizes a vector */
-INLINE void v_norm(pVector a) {
+CTL_INLINE void v_norm(pVector a) {
     double l = sqrt(vv_dot(a, a));
     if(fabs(l) > 0.0001) vc_div(a, l);
 }
 
 /* v_neg negates a vector (opposite direction) */
-INLINE void v_neg(pVector a) {
+CTL_INLINE void v_neg(pVector a) {
     a[0] = -a[0];
     a[1] = -a[1];
     a[2] = -a[2];
 }
 
 /* vm_mul multiplies a vector by a 4x4 matrix, and puts the result in a */
-INLINE void vm_mul(pVector a, const pMatrix b) {
+CTL_INLINE void vm_mul(pVector a, const pMatrix b) {
     Vector c;
     vv_cpy(c, a);
     a[0] = c[0]*b[0] + c[1]*b[4] + c[2]*b[8] + c[3]*b[12];
